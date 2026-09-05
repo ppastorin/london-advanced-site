3305138b291243fd63f2a18001290866cb8d1eee
ppastorin <43879305+ppastorin@users.noreply.github.com>
2026-09-05 17:01:07 +0100

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -94,7 +94,10 @@ function loadGoogleAnalytics(id) {
   window.dataLayer = window.dataLayer || [];
   window.gtag = function(){ window.dataLayer.push(arguments); };
   window.gtag("js", new Date());
-  window.gtag("config", id, { anonymize_ip: true });
+ window.gtag("config", id, {
+  anonymize_ip: true,
+  send_page_view: false
+});
   const script = document.createElement("script");
   script.async = true;
   script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(id)}`;
